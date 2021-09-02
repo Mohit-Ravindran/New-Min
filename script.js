2fdac7ebac499dbdd3f60ce8bde7d442a5d9ac75
Mohit-Ravindran <89247601+Mohit-Ravindran@users.noreply.github.com>
2021-09-02 10:54:12 +0530

    New

--- script.js
+++ script.js
@@ -2,14 +2,27 @@ function ageindays() {
 
     var birthyear = prompt("What is your name?");
     // var bindays = (2021 - birthyear) * 365;
-    var bindays = "Mohit Ravindran";
+    if (birthyear === "anusha" || birthyear === "Anusha") {
+        var bindays = "Mohit Ravindran";
     var h1 = document.createElement('h1');
     var textanswer = document.createTextNode( 'Your soulmates name is ' + bindays + ' and he loves you so very much' + '');
     h1.setAttribute('id', 'ageindays');
     h1.appendChild(textanswer);
     document.getElementById('flex-result').appendChild(h1);
+     }
+
+ else{
+    var bindays = "Mohit Ravindran";
+    var h1 = document.createElement('h1');
+    var textanswer = document.createTextNode( 'Invalid Soul');
+    h1.setAttribute('id', 'ageindays');
+    h1.appendChild(textanswer);
+    document.getElementById('flex-result').appendChild(h1);
+     
+    
 }
 
-function reset() {
-    document.getElementById("ageindays").remove();
-}
+
+
+ }
+    
